3ba028be674bfd144d1954bd7c9a6eb39da03088
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-06-10 20:30:19 +0200

    added image recoloring function both with and without multithreading

---
+++ src/main/java/com/s3/multi_threading_couse/_6_ImageRecoloringAlgorithm.java
@@ -0,0 +1,147 @@
+package com.s3.multi_threading_couse;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _6_ImageRecoloringAlgorithm {
+
+    //public static final String SOURCE_FILE = "D:\\Projects\\multi_threading_course\\src\\main\\resources\\many-flowers.jpg";
+    public static final String SOURCE_FILE = "D:\\Projects\\multi_threading_course\\src\\main\\resources\\ferrari-8k.jpg";
+    //public static final String OUTPUT_FILE = "D:\\Projects\\multi_threading_course\\target\\classes\\many-flowers.jpg";
+    public static final String OUTPUT_FILE = "D:\\Projects\\multi_threading_course\\target\\classes\\ferrari-8k.jpg";
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+        BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        long startTime = System.currentTimeMillis();
+        //recolorSingleThreaded(originalImage, resultImage);
+        recolorMultiThreaded(originalImage, resultImage, 8);
+        long endTime = System.currentTimeMillis();
+
+        long totalDuration = endTime - startTime;
+
+        File outputFile = new File(OUTPUT_FILE);
+        ImageIO.write(resultImage, "JPG", outputFile);
+
+        System.out.println(String.format("Recoloring the image took: %d miliseconds", totalDuration));
+    }
+
+    public static void recolorMultiThreaded(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads) {
+        List<Thread> threads = new ArrayList<>();
+
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight() / numberOfThreads;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadMultiplier = i;
+
+            Thread thread = new Thread(() -> {
+                int leftCorner = 0;
+                int topCorner = height * threadMultiplier;
+
+                recolorImage(originalImage, resultImage, leftCorner, topCorner, width, height);
+            });
+
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner, int width, int height) {
+        // Loop through all pixels in the image
+        for (int x = leftCorner; x < leftCorner + width && x < originalImage.getWidth(); x++) {
+            for (int y = topCorner; y < topCorner + height && y < originalImage.getHeight(); y++) {
+                recolorPixel(originalImage, resultImage, x, y);
+            }
+        }
+    }
+
+    public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
+        int rgb = originalImage.getRGB(x, y);
+
+        int red = getRed(rgb);
+        int green = getGreen(rgb);
+        int blue = getBlue(rgb);
+
+        int newRed;
+        int newGreen;
+        int newBlue;
+
+        // Mostly hard coded value to create a purplish color
+        if (isShadeOfGrey(red, green, blue)) {
+            newRed = Math.min(255, red + 10);
+            newGreen = Math.max(0, green - 80);
+            newBlue = Math.max(0, blue - 20);
+        } else {
+            newRed = red;
+            newGreen = green;
+            newBlue = blue;
+        }
+
+        int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+
+        setRGB(resultImage, x, y, newRGB);
+    }
+
+    /**
+     * Input the target image, pixel location and the rgb value.
+     * Set the pixel in the new image as the given rgb value
+     */
+    public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+        image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+    }
+
+    /**
+     * Look how well the colors are mixed
+     * If they are mixed very equally, then it's a shade of grey (white through black)
+     */
+    public static boolean isShadeOfGrey(int red, int green, int blue) {
+        return Math.abs(red - green) < 30 && Math.abs(red - blue) < 30 && Math.abs(green - blue) < 30;
+    }
+
+    public static int createRGBFromColors(int red, int green, int blue) {
+        int rgb = 0;
+
+        rgb |= blue;
+        rgb |= green << 8;
+        rgb |= red << 16;
+
+        // Set opacity at full 255
+        rgb |= 0xFF000000;
+
+        return rgb;
+    }
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getBlue(int rgb) {
+        return rgb & 0x000000FF;
+    }
+}
